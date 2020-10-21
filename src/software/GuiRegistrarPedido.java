6478e65bba538fe162c14837dd58c2f1cf43d477
danivillegas17 <73149829+danivillegas17@users.noreply.github.com>
2020-10-21 16:26:59 -0500

    botones guardar, añadir, limpiar
    3 botones sin acciones

--- src/software/GuiRegistrarPedido.java
+++ src/software/GuiRegistrarPedido.java
@@ -6,15 +6,15 @@ import java.awt.event.*;
 
 public class GuiRegistrarPedido extends JFrame {
 
-    JTextField CodigoPedido, FechaPedido, FechaEntrega, cliente, postre, LugarEntrega, ValorTotal;
+    JTextField CodigoPedido, FechaPedido, FechaEntrega, LugarEntrega, ValorTotal;
     JComboBox Cliente, Postre;
     JRadioButton Tienda, Domicilio;
     ButtonGroup TiendaDomicilio;
-    JButton guardarPedidoButton, añadirPostreButton, limpiarCamposButton;
+    JButton GuardarPedidoButton, AñadirPostreButton, LimpiarCamposButton;
 
-    public GuiRegistrarPedido() { 
-        
-         setTitle("Registrar Pedido");
+    public GuiRegistrarPedido() {
+
+        setTitle("Registrar Pedido");
         setLayout(new FlowLayout(FlowLayout.LEFT, 10, 10));
 
         add(new JLabel("Codigo del pedido:"));
@@ -27,29 +27,44 @@ public class GuiRegistrarPedido extends JFrame {
         add(FechaEntrega = new JTextField(20));
 
         add(new JLabel("Cliente:"));
-        add(cliente = new JTextField(27));
+        Cliente = new JComboBox();
+        Cliente.addItem("Cliente 1");
+        Cliente.addItem("Cliente 2");
+        add(Cliente);
 
         add(new JLabel("Postre:"));
-        add(postre = new JTextField(28));
+        Postre = new JComboBox();
+        Postre.addItem("Postre 1");
+        Postre.addItem("Postre 2");
+        add(Postre);
 
         add(new JLabel("Lugar entrega:"));
         add(LugarEntrega = new JTextField(20));
 
         add(new JLabel("Valor Total:"));
         add(ValorTotal = new JTextField(26));
-        
-                add(new JLabel("Tipo de lugar:"));
-		TiendaDomicilio= new ButtonGroup();
-		add(Tienda = new JRadioButton("Tienda"));
-		add(Domicilio = new JRadioButton("Domicilio"));
-		TiendaDomicilio.add(Tienda);
-		TiendaDomicilio.add(Domicilio);
 
-       
+        add(new JLabel("Tipo de lugar:"));
+        TiendaDomicilio = new ButtonGroup();
+        add(Tienda = new JRadioButton("Tienda"));
+        add(Domicilio = new JRadioButton("Domicilio"));
+        TiendaDomicilio.add(Tienda);
+        TiendaDomicilio.add(Domicilio);
+
+        add(GuardarPedidoButton = new JButton("Guardar Pedido"));
+        GuardarPedidoButton.addActionListener(new GuardarPedido());
+
+        add(AñadirPostreButton = new JButton("Añadir Postre"));
+        AñadirPostreButton.addActionListener(new AñadirPostre());
+
+        add(LimpiarCamposButton = new JButton("Limpiar Campos"));
+        LimpiarCamposButton.addActionListener(new LimpiarCampos());
+
         setSize(400, 500);
         setResizable(false);
         setLocationRelativeTo(null);
         setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
 
     }
 }
