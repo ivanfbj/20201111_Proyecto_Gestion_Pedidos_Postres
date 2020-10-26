dbcfad06406abc923e9daa584eafc234d3380150
ivanfbj <67565041+ivanfbj@users.noreply.github.com>
2020-10-25 22:25:46 -0500

    Validación para no guardar postres sin datos
    - Crear validación para que no se pueda guardar un postre con datos vacios
    - Modificación en los mensajes de alerta

--- src/software/GuiRegistrarCliente.java
+++ src/software/GuiRegistrarCliente.java
@@ -80,7 +80,9 @@ public class GuiRegistrarCliente extends JFrame {
 
 			if (nombreCliente.getText().equals("") || direccion.getText().equals("") || telefono.getText().equals("") || edad.getText().equals("")
 					|| (masculino.isSelected() == false & femenino.isSelected() == false)) {
-				JOptionPane.showMessageDialog(null, "Por favor verificar que todos los campos esten diligenciados");
+				
+				JOptionPane.showMessageDialog(null, "Por favor verificar que todos los campos esten diligenciados","FALTA INFORMACIÓN", JOptionPane.INFORMATION_MESSAGE);
+			
 			} else {
 
 				cliente.registrarCliente(nombreCliente.getText(), direccion.getText(), Integer.parseInt(telefono.getText()), sexo.getSelection().getActionCommand(), Integer.parseInt(edad.getText()));
@@ -105,7 +107,6 @@ public class GuiRegistrarCliente extends JFrame {
 			edad.setText(null);
 			sexo.clearSelection();
 
-			JOptionPane.showMessageDialog(null, "Clic en el boton Limpiar campos");
 
 		}
 	}
