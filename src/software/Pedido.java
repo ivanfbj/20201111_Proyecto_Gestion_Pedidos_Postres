d52461b3fcd2f0e145acae4435197b837b516eb3
ivanfbj <67565041+ivanfbj@users.noreply.github.com>
2020-10-30 21:26:04 -0500

    Comentarear lineas de código Pedido

--- src/software/Pedido.java
+++ src/software/Pedido.java
@@ -9,9 +9,9 @@ public class Pedido {
 	private Postre[] postre;
 	private String lugarDeEntrega;
 	private double valorTotal;
-	private int cantpostres;// contador
+	// private int cantpostres;
 
-	public Pedido(String codigoPedido, String fechaPedido, String fechaEntregaPedido, Cliente cliente, String lugarDeEntrega, double valorTotal, int p) {
+	public Pedido(String codigoPedido, String fechaPedido, String fechaEntregaPedido, Cliente cliente, String lugarDeEntrega, double valorTotal/* , int p */) {
 		this.codigoPedido = codigoPedido;
 		this.fechaPedido = fechaPedido;
 		this.fechaEntregaPedido = fechaEntregaPedido;
@@ -19,37 +19,37 @@ public class Pedido {
 		this.lugarDeEntrega = lugarDeEntrega;
 		this.valorTotal = valorTotal;
 
-		postre = new Postre[p];
-		cantpostres = 0;
+//		postre = new Postre[p];
+//		cantpostres = 0;
 
 	}
 
-	public void agregarPotre(Postre recibirpostre) {
-		if (cantpostres < postre.length) {
-			postre[cantpostres] = recibirpostre;
-			cantpostres++;
-		}
-	}
+//	public void agregarPotre(Postre recibirpostre) {
+//		if (cantpostres < postre.length) {
+//			postre[cantpostres] = recibirpostre;
+//			cantpostres++;
+//		}
+//	}
 
-	public void calcularvalortotal() {
-		double ContTotal = 0;
+//	public void calcularvalortotal() {
+//		double ContTotal = 0;
+//
+//		for (int i = 0; i < cantpostres; i++) {
+//			// sumar la cantidad del valir de los postres y (asignar resultado del acumulador en totalValor(porfuera del ciclo))
+//		}
+//	}
 
-		for (int i = 0; i < cantpostres; i++) {
-			// sumar la cantidad del valir de los postres y (asignar resultado del acumulador en totalValor(porfuera del ciclo))
-		}
-	}
-
-	@Override
-	public String toString() {
-		String cadena = "";
-		cadena = "codigoPedido=" + codigoPedido + ", fechaPedido=" + fechaPedido + ", fechaEntregaPedido=" + fechaEntregaPedido + ", cliente=" + cliente + ", postre=" + postre + ", lugarDeEntrega="
-				+ lugarDeEntrega + ", valorTotal=" + valorTotal;
-		for (int i = 0; cantpostres >= i; i++) {
-			cadena = "\ncantidad de postres\n";
-			cadena += postre[i].toString();
-			// se organiza el error para recorre el vector postre
-
-		}
-		return cadena;
-	}
+//	@Override
+//	public String toString() {
+//		String cadena = "";
+//		cadena = "codigoPedido=" + codigoPedido + ", fechaPedido=" + fechaPedido + ", fechaEntregaPedido=" + fechaEntregaPedido + ", cliente=" + cliente + ", postre=" + postre + ", lugarDeEntrega="
+//				+ lugarDeEntrega + ", valorTotal=" + valorTotal;
+//		for (int i = 0; cantpostres >= i; i++) {
+//			cadena = "\ncantidad de postres\n";
+//			cadena += postre[i].toString();
+//			// se organiza el error para recorre el vector postre
+//
+//		}
+//		return cadena;
+//	}
 }
